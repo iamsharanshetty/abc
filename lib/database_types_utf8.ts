42e2627ebd8d60c04af74014099598a384a04c59
Nagarjun-07 <nagarjunh77@gmail.com>
2025-12-15 23:10:41 +0530

    ALE-55: add analytics event logging

---
+++ lib/database_types_utf8.ts
@@ -0,0 +1,93 @@
+﻿export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[]
+
+export interface Database {
+  public: {
+    Tables: {
+      agents: {
+        Row: {
+          id: string
+          created_at: string
+          user_id: string
+          name: string
+          role: string
+          status: string
+          settings: Json
+        }
+        Insert: {
+          id?: string
+          created_at?: string
+          user_id: string
+          name: string
+          role: string
+          status?: string
+          settings?: Json
+        }
+        Update: {
+          id?: string
+          created_at?: string
+          user_id?: string
+          name?: string
+          role?: string
+          status?: string
+          settings?: Json
+        }
+        Relationships: [
+          {
+            foreignKeyName: "agents_user_id_fkey"
+            columns: ["user_id"]
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+      website_embeddings: {
+        Row: {
+          content_section: string
+          created_at: string
+          embedding: string
+          id: number
+          metadata: Json
+          page_url: string
+          website_url: string
+        }
+        Insert: {
+          content_section: string
+          created_at?: string
+          embedding: string
+          id?: number
+          metadata?: Json
+          page_url: string
+          website_url: string
+        }
+        Update: {
+          content_section?: string
+          created_at?: string
+          embedding?: string
+          id?: number
+          metadata?: Json
+          page_url?: string
+          website_url?: string
+        }
+        Relationships: []
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      [_ in never]: never
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
+}
